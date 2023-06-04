8f301baa5ac17d867d66721978d58dee2ce65e92
Satyam Gupta <satyam.gupta756@gmail.com>
2023-06-05 02:49:49 +0530

    Deleted Everything to start from scratch

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/700.css';
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
